c4b73d1c64e2991fa391f36b630e5627d6a774a0
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-12-13 18:22:28 +0100

    rendering users list in nextjs with SSR and CSR

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -5,6 +5,12 @@ import MainRouter from "./routes";
 
 const app = express();
 app.use(helmet());
+// TODO: merge below functionality into above helmet config
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 
 app.get("/", (req, res) => {
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,59 @@
 import { Button } from "ui";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { GetServerSideProps } from "next";
 
-export default function Web() {
+interface User {
+  id: number | string;
+  email: string;
+  hashedPassword: string;
+  firstName: string;
+  lastName: string;
+  birthdate: string;
+}
+
+export default function Web({ initialUsers }: { initialUsers: User[] }) {
+  // Client Side Rendering: The useState and useEffect will be executed in the React
+  // app after loading the page once client JS takes over.
+  const [users, setUsers] = useState<User[]>(initialUsers);
+  useEffect(() => {
+    axios
+      .get<User[]>("http://localhost:5050/api/users")
+      .then((response) => response.data)
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      <ul>
+        {users.map((user) => (
+          <li>
+            <ul>
+              <li>id: {user.id}</li>
+              <li>email: {user.email}</li>
+              <li>hashedPassword: {user.hashedPassword}</li>
+              <li>firstName: {user.firstName}</li>
+              <li>lastName: {user.lastName}</li>
+              <li>birthdate: {user.birthdate}</li>
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+// Server Side Rendering: we provide the props to the component that are
+// used to prerender before the client sees the page.
+export const getServerSideProps: GetServerSideProps = async () => {
+  const users = await axios
+    .get<User[]>("http://localhost:5050/api/users")
+    .then((response) => response.data);
+  return {
+    props: {
+      initialUsers: users
+    },
+  };
+};
