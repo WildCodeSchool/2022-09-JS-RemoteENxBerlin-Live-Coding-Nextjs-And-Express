4a856cc6908721940ab279bcdaa387c54d733f6c
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-12-13 18:23:54 +0100

    added sql to initialize tables

---
+++ apps/backend/init-tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE
+    users (
+        id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        email VARCHAR(255) NOT NULL,
+        hashedPassword VARCHAR(255),
+        firstName VARCHAR(100),
+        lastName VARCHAR(100),
+        birthdate DATE
+    );
